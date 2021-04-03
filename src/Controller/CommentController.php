df2a6e40abc93a61d46bf2709b6c3345006678e4
Saturnin Dao <saturnindao@gmail.com>
2021-04-03 15:20:56 +0000

    we setup our API  endpoint for votes

---
+++ src/Controller/CommentController.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CommentController extends AbstractController
+{
+
+    /**
+     * @Route("comments/{id}/vote/{direction}")
+     */
+    public function commentVote($id,$direction){
+
+        //todo use id to query database
+
+        // use a real logic here to save this to the database
+        if ($direction==='up'){
+            $currentVoteCount = rand(7,100);
+        }else{
+            $currentVoteCount = rand(0, 5);
+        }
+
+        return $this->json(['votes'=> $currentVoteCount]);
+    }
+}
