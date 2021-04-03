0460cf5faa8b776f5f3814d6e568d863d469ebfc
Saturnin Dao <saturnindao@gmail.com>
2021-04-03 21:48:38 +0000

    caching question texte

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -4,9 +4,12 @@
 namespace App\Controller;
 
 
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
+
 
 class QuestionController extends AbstractController
 {
@@ -21,10 +24,18 @@ class QuestionController extends AbstractController
 
     /**
      * @Route("/questions/{slug}", name="app_question_show")
+     * @param $slug
+     * @param MarkdownParserInterface $markdownParser
+     * @param CacheInterface $cache
+     * @return Response
      */
-    public function show($slug){
+    public function show($slug, MarkdownParserInterface $markdownParser, CacheInterface $cache){
 
         $questionText = "I've been turned into a cat, any thoughts on how to turn back? While I'm **adorable**, I don't really care for cat food.";
+
+        $parsedQuestionText = $cache->get('markdown_'.md5($questionText),function() use ($questionText, $markdownParser){
+            return $markdownParser->transformMarkdown($questionText);
+        });
 
         $answers = [
             'Make sure your cat is sitting percfectly',
