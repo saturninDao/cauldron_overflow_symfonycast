d878e1b22c434926ab4b292cc174e40864df9be2
Saturnin Dao <saturnindao@gmail.com>
2021-04-19 06:10:50 +0000

    Inserting our fist quesiton in the database

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -4,9 +4,10 @@
 namespace App\Controller;
 
 
+use App\Entity\Question;
 use App\Service\MarkdownHelper;
+use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
-use Sentry\State\HubInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -43,6 +44,37 @@ class QuestionController extends AbstractController
     }
 
     /**
+     * @Route("/questions/new")
+     */
+    public function new(EntityManagerInterface $entityManager)
+    {
+        $question = new Question();
+        $question->setName('Missing pants')
+            ->setSlug('missing-pants-'.rand(0, 1000))
+            ->setQuestion(<<<EOF
+Hi! So... I'm having a *weird* day. Yesterday, I cast a spell
+to make my dishes wash themselves. But while I was casting it,
+I slipped a little and I think `I also hit my pants with the spell`.
+When I woke up this morning, I caught a quick glimpse of my pants
+opening the front door and walking out! I've been out all afternoon
+(with no pants mind you) searching for them.
+Does anyone have a spell to call your pants back?
+EOF
+            );
+        if (rand(1, 10) > 2) {
+            $question->setAskedAt(new \DateTime(sprintf('-%d days', rand(1, 100))));
+        }
+
+        $entityManager->persist($question);
+        $entityManager->flush();
+        return new Response(sprintf(
+            'Well hallo! The shiny new question is id #%d, slug: %s',
+            $question->getId(),
+            $question->getSlug()
+        ));
+    }
+
+    /**
      * @Route("/questions/{slug}", name="app_question_show")
      * @param $slug
      * @param MarkdownHelper $markdownHelper
@@ -69,5 +101,6 @@ class QuestionController extends AbstractController
             'questionText' => $questionText
             ]);
     }
+
 
 }
